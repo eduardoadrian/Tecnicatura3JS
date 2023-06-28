309453cc3db17c87fad6c8aa35095435fd52d991
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-06-28 08:38:57 -0300

    TSLeccion05-Clase8-Static

--- TSLeccion05/05-03-clases.js
+++ TSLeccion05/05-03-clases.js
@@ -1,9 +1,24 @@
 //let persona3 = new persona('Carla, 'Ponce); esto no se debe hacer: Persona is not defined
 
 class Persona{//clase padre
+
+    static contadorPersonas = 0; //Atributo estatico
+    //email = 'Valor default email'; //Atributo no estático
+
+    static get MAX_OBJ(){ //Este método simula una constante
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersona;
+        }
+        else{
+            console.log('Se ha superado el maximo de objetospermitidos');
+        }
+        //console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -23,8 +38,25 @@ class Persona{//clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this.idPersona+' '+this._nombre+' '+this._apellido;
     }
+
+    //Sobreescribiendo el método de la clase padre (object)
+    toString(){ //Regresa un String
+        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
+        //El método que se ejecuta depende si es una referencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
+
+    static saludar(){
+        console.log('Saludos desde este método static');
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre+' '+persona.apellido);
+    }
+
+
 }
 
 class Empleado extends Persona{ //Clase hija
@@ -40,16 +72,9 @@ class Empleado extends Persona{ //Clase hija
     set departamento(departamento){
         this._departamento = departamento;
     }
-
     //Sobreescritura
     nombreCompleto(){
         return super.nombreCompleto()+', '+this._departamento;
-    }
-    //Sobreescribiendo el método de la clase padre (object)
-    toString(){ //Regresa un String
-        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
-        //El método que se ejecuta depende si es una referencia de tipo padre o hija
-        return this.nombreCompleta();
     }
 }
 let persona1 = new Persona('Martin', 'Perez');
@@ -71,8 +96,36 @@ console.log(empleado1.nombreCompleto());
 console.log(empleado1.toString());
 console.log(persona1.toString());
 
+//persona1.saludar(); no se utiliza desde el objeto
+Persona.saludar();
+Persona.saludar2(persona1);
 
+Empleado.saludar();
+Empleado.saludar2(empleado1);
 
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email); No se puede acceder desde la clase
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona('Carla','Pertosi');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+//Persona.MAX_OBJ = 10; No se puede modificar, ni alterar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', 'Diaz');
+console.log(persona4.toString());
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
 
 //Tarea
 //Let persona1 = new Persona('Martin', 'Perez')
